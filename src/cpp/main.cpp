f67fae32d9e772000fc9b917b9eacb5d37f1c728
Andreas Larsen <northcode.no@gmail.com>
2017-01-29 21:45:33 +0100

    Cleanup main()

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -17,36 +17,12 @@ int main() {
   cout << "string: " << sizeof(std::string) << endl
        << "token:  " << sizeof(token) << endl;
 
-  // mystr teststr = arr.alloc_str_from("hello, world");
-
-
-  // cout << "Message:" << teststr << endl;
-  // arr.append_char(&teststr,'c');
-  // cout << "Message:" << teststr << endl;
-  // arr.append_str(&teststr," testing");
-  // cout << "Message:" << teststr << endl;
-
-  // arr.delete_head(&teststr);
-
-  // mystr test2 = arr.alloc_str_from("this is another str");
-
-  // cout << "discarded: " << teststr << endl;
-  // cout << "new alloc: " << test2 << endl;
-
-  // stringstream ss{"this is a test 2h + 4; *a = 50;"};
 
   const char* input = "45 hello 'c' \"hello,\\n world\" 3.14159";
   
   stringstream ss{input};
 
   parser p{&arr,ss};
-
-  // p.scan_token();
-  // p.scan_token();
-  // p.scan_token();
-  // p.scan_token();
-  // p.scan_token();
-  // p.scan_token();
 
   cout << "scanning: " << input << endl;
 
